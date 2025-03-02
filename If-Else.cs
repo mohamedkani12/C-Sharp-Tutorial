12a8a1f3a717c0dda1d4aab79048a171bd25ec5e
Mohamed Kani P K <145331483+mohamedkani12@users.noreply.github.com>
2025-03-02 15:38:46 +0530

    If-Else statement checking condition

---
+++ If-Else.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFElse
+{
+    class If_Else
+    {
+        static void Main(String[] args)
+        {
+            int number1;
+            Console.Write("Enter number1 : ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            int number2;
+            Console.Write("Enter number2 : ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Value of "+number1+" X "+ number2 + ":" );
+            int userValue = Convert.ToInt32(Console.ReadLine());
+            int actualValue = number1 * number2;
+            if (userValue == actualValue)
+            {
+                Console.WriteLine("You are correct");
+            }
+            else
+            {
+                Console.WriteLine("You are wrong");
+            }
+        }
+    }
+}

d91b99c7c446ed288867b52c5ed36e365ac2564b
Mohamed Kani P K <145331483+mohamedkani12@users.noreply.github.com>
2025-03-02 15:26:35 +0530

    If - Else statement

--- UserInput.cs
+++ UserInput.cs
@@ -11,11 +11,24 @@ namespace GettingInputFromUser
         static void Main(String[] args)
         {
             int age;
+            Console.Write("Enter age :");
             age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Age of the Person : "+age);
             string name;
+            Console.Write("Enter Name : ");
             name = Console.ReadLine();
-            Console.WriteLine("Name of the Person : "+name);
+            Console.WriteLine("Name of the Person : " + name);
+            Console.WriteLine("Age of the Person : " + age);
+            if(age>=18 && age <= 25)
+            {
+                Console.WriteLine("Your age is between 18 and 25");
+            }
+            else if (age > 25 && age < 150)
+            {
+                Console.WriteLine("your age is older than 25");
+            }
+            else if(age<0 || age>150){
+                Console.WriteLine("Invalid age");
+            }
         }
     }
 }

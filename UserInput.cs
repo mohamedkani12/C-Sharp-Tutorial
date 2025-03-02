161b8faf679755502c0edcea06b8fc46e257a5f1
Mohamed Kani P K <145331483+mohamedkani12@users.noreply.github.com>
2025-03-02 15:12:59 +0530

    Getting input from User

---
+++ UserInput.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingInputFromUser
+{
+    class UserInput
+    {
+        static void Main(String[] args)
+        {
+            int age;
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Age of the Person : "+age);
+            string name;
+            name = Console.ReadLine();
+            Console.WriteLine("Name of the Person : "+name);
+        }
+    }
+}

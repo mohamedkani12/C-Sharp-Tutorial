c9741a3d26ac46b75bbad94eb990eb40431f1b50
Mohamed Kani P K <145331483+mohamedkani12@users.noreply.github.com>
2025-03-02 21:43:10 +0530

    convert string into interger using int.TryParse

---
+++ TryParse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingTryParse
+{
+    class TryParse
+    {
+        static void Main(String[] args)
+        {
+            bool success = true;
+            while (success)
+            {
+                Console.Write("Enter a number :  ");
+                string input = Console.ReadLine();
+                if(int.TryParse(input,out int num))
+                {
+                    success = false;
+                    Console.WriteLine(num);
+                }
+                else
+                {
+                    const string message = "Invalid input, please enter a number";
+                    Console.WriteLine(message);
+                }
+            }
+        }
+    }
+}

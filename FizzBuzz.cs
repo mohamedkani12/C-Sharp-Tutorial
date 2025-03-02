60561f50c29bde935d4c9c2c206ab7f19790fef2
Mohamed Kani P K <145331483+mohamedkani12@users.noreply.github.com>
2025-03-02 22:21:40 +0530

    fizz & buzz

---
+++ FizzBuzz.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzGame
+{
+    class FizzBuzz
+    {
+        static void Main(String[] args)
+        {
+            bool threeDiv = false;
+            bool fiveDiv = false;
+            for (int i = 1; i <= 15; i++)
+            {
+                threeDiv = i % 3 == 0;
+                fiveDiv = i % 5 == 0;
+                if (threeDiv && fiveDiv)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (threeDiv)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (fiveDiv)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
